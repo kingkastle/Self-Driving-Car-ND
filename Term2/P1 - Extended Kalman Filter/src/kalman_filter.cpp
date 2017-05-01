4f4dce99bacb40d7926ae2590327448a5533b2cb
Rafael Castillo Alcibar <rafael.castillo.alcibar@bbva.com>
2017-05-01 11:33:03 +0200

    Extended KF first submission

---
+++ Term2/P1 - Extended Kalman Filter/src/kalman_filter.cpp
@@ -0,0 +1,96 @@
+#include "kalman_filter.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <stdlib.h>
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //  new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+  VectorXd Hx = VectorXd(3);
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+  float rho = sqrt(pow(px, 2) + pow(py, 2));
+  float phi = 0.0; // this will be default case if px is 0
+  if (fabs(px) > 0.0001) {
+      phi = atan2(py,px);
+  }
+  float rho_dot = 0; // this will be default case if rho is 0
+  if (fabs(rho) > 0.0001) {
+      rho_dot = (px*vx + py*vy) / rho;
+  }
+  Hx << rho, phi, rho_dot;
+  VectorXd y = z - Hx;
+  // normalize result to -pi and pi
+  if ((y(1)) > 3.14 / 2.){
+      y(1) = y(1) - 2 * 3.14;
+  }
+  if ((y(1)) < -3.14 / 2.){
+      y(1) = y(1) + 2 * 3.14;
+  }
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //  new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
